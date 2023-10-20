3898f4ff1f6de038fb61c8dc32d38b69e7c60a44
Arturo Salazar <asalazar@hawk.iit.edu>
2023-10-20 17:58:02 -0500

    add game loop and prompt for user

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -5,3 +5,9 @@ computerPossibleMoves = ("rock", "paper", "scissors")
 computerMove = computerPossibleMoves[randint(0,2)]
 print(computerMove)
 
+#Game Start Loop
+keepPlaying = True
+while (keepPlaying):
+    #Select a move 
+    userMove = input("Select a move: r, p, s: ")
+    print(userMove)
