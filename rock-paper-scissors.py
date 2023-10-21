9af376fec27eeef4f7610a4ece958000b1dd9414
Arturo Salazar <asalazar@hawk.iit.edu>
2023-10-20 20:10:35 -0500

    Add code to check userMove against computerMove

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -6,6 +6,8 @@ while (keepPlaying):
     #Randomly select 
     computerPossibleMoves = ("Rock", "Paper", "Scissors")
     computerMove = computerPossibleMoves[randint(0,2)]
+    #Test - select specific computerMove
+    computerMove = "Scissors"
 
     #Select a move 
     userMove = input("Select a move: Rock, Paper, Scissors: ")
@@ -25,3 +27,26 @@ while (keepPlaying):
     print("Computer selected: " + computerMove.title())
     print("User selected: " + userMove.title())
     
+    #Compare the user's choice with the computer's choice to determine the winner.
+    #Tie Condition
+    if userMove == computerMove:
+        print("It's a tie!")
+
+    #Player Chooses Rock
+    elif userMove == "Rock":
+        if computerMove =="Scissors":
+            print(f"You win! {userMove} smashes {computerMove}")
+        elif computerMove == "Paper":
+            print(f"Computer wins! {computerMove} covers {userMove}")
+    #Player Chooses Paper
+    elif userMove == "Paper":
+        if computerMove =="Rock":
+            print(f"You win! {userMove} covers {computerMove}")
+        elif computerMove == "Scissors":
+            print(f"Computer wins! {computerMove} cuts {userMove}")
+    #Player Chooses Scissors
+    elif userMove == "Scissors":
+        if computerMove =="Paper":
+            print(f"You win! {userMove} cuts {computerMove}")
+        elif computerMove == "Rock":
+            print(f"Computer wins! {computerMove} smashes {userMove}")
