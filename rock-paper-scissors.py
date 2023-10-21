7c494ca95e2a5e3c1086de8f1e2dcab771bd70fd
Arturo Salazar <asalazar@hawk.iit.edu>
2023-10-20 20:01:50 -0500

    Refactor options to capitalize first letter and print options

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,7 +1,7 @@
 from random import randint
 
 #Randomly select 
-computerPossibleMoves = ("rock", "paper", "scissors")
+computerPossibleMoves = ("Rock", "Paper", "Scissors")
 computerMove = computerPossibleMoves[randint(0,2)]
 print(computerMove)
 
@@ -9,17 +9,21 @@ print(computerMove)
 keepPlaying = True
 while (keepPlaying):
     #Select a move 
-    userMove = input("Select a move: rock, paper, scissors: ")
+    userMove = input("Select a move: Rock, Paper, Scissors: ")
     print(userMove)
 
     #Convert move to avoid formatting problems
     userMove = userMove.lower() #make lowercase
+    userMove = userMove.title() #capitalize first letter
     userMove = "".join(userMove.split()) #remove all whitespace
     print(userMove)
 
     #Check user input
-    if userMove not in ["rock", "paper", "scissors"]:
+    if userMove not in ["Rock", "Paper", "Scissors"]:
         print("Incorrect Selection, try again")
         continue
+
+    print("Computer selected: " + computerMove.title())
+    print("User selected: " + userMove.title())
 
     
